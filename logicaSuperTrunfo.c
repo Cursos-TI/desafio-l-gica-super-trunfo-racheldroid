e81994e35ff31adde22fdb616defd8e98d9a5f90
racheldroid <72256494+racheldroid@users.noreply.github.com>
2025-02-11 15:50:20 +0000

    11-02-25

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -16,9 +16,9 @@ int main() {
         float populacao1;
         float area1;
         float Densidade_populacional1;
-        double PIB1;
-        double PIB_per_capta1 ;
-        int numero_de_pontos_turisticos1;
+        float PIB1;
+        float PIB_per_capta1 ;
+        float numero_de_pontos_turisticos1;
         
 
 
@@ -29,9 +29,9 @@ int main() {
         float populacao2;
         float area2;
         float Densidade_populacional2;
-        double PIB2;
-        double PIB_per_capta2 ;
-        int numero_de_pontos_turisticos2;
+        float PIB2;
+        float PIB_per_capta2 ;
+        float numero_de_pontos_turisticos2;
     
     // Cadastro das Cartas:
     // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
@@ -45,7 +45,8 @@ int main() {
     // (Repita para cada propriedade)
 
         // Cadastro da primeira carta
-
+        printf ("****BEM VINDO AO JOGO SUPER TRUNFO****");
+        puts ("");
         printf("***Cadastro da primeira carta***\n");
         printf("Digite o estado: ");
         scanf("%s", Estado1 );
@@ -63,10 +64,10 @@ int main() {
         scanf("%f", &area1 );
         
         printf("Digite o PIB da cidade: ");
-        scanf("%lf", &PIB1 );   
+        scanf("%f", &PIB1 );   
 
         printf("Digite o número de pontos turisticos da cidade:");
-        scanf("%d", &numero_de_pontos_turisticos1 );
+        scanf("%f", &numero_de_pontos_turisticos1 );
         puts("");
 
         // Cadastro da segunda carta
@@ -88,13 +89,12 @@ int main() {
         scanf("%f", &area2 );
         
         printf("Digite o PIB da cidade: ");
-        scanf("%lf", &PIB2 );   
+        scanf("%f", &PIB2 );   
 
         printf("Digite o número de pontos turisticos da cidade:");
-        scanf("%d", &numero_de_pontos_turisticos2 );
-        puts("");
+        scanf("%f", &numero_de_pontos_turisticos2 );
 
-        /*
+                /*
          Cálculo de Densidade Populacional: População dividida pela área da cidade.
         e
         PIB per Capita: PIB total dividido pela população.
@@ -107,7 +107,49 @@ int main() {
         Densidade_populacional2 = populacao2 / area2;
         PIB_per_capta2 = PIB2 / populacao2;
 
+    //Mostrando as informações calculadas com a informaçoes de entrada
+        puts("");
+        printf ("Processando...\n");
+        puts("");
+        printf ("Foi apurado que:\n");
+        printf ("A densidade populacional de %s é: %.2f \n", Cidade1, Densidade_populacional1);
+        printf ("O PIB per capta de %s é: %.2f \n", Cidade1, PIB_per_capta1);
+        printf ("A densidade populacional de %s é: %.2f \n", Cidade2, Densidade_populacional2);
+        printf ("O PIB per capta de %s é: %.2f \n", Cidade2, PIB_per_capta2);
+        puts("");
 
+    // Agora vamos jogar
+        printf ("Vamos Jogar?\n");
+        printf ("Jogadores, escolham sabiamente, qual propriedade vocês querem comparar?\n");
+        printf ("Digite o número da opção.\n");
+        printf ("1. População\n");
+        printf ("2. Área\n");
+        printf ("3. PIB\n");
+        printf ("4. Número de pontos turísticos\n");
+        printf ("5. Densidade populacional\n");
+        printf ("6. PIB per capta\n");
+        puts ("");
+
+        int EscolhaJogador1;
+        int EscolhaJogador2;
+
+        printf ("Escolha do jogador 1:");
+        scanf ("%d", &EscolhaJogador1);
+        puts ("");
+        printf ("Escolha do jogador 2:");
+        scanf ("%d", &EscolhaJogador2);
+
+        switch (EscolhaJogador1)
+        {
+        case 1:
+            printf ("")
+            break;
+        
+        default:
+            break;
+        }
+
+/*
     // Comparação de Cartas:
     // Desenvolva a lógica de comparação entre duas cartas.
     // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
@@ -118,7 +160,7 @@ int main() {
         printf ("Foi apurado que:\n");
 
     // Exemplo:
-        if (populacao1 > populacao2) {
+            if (populacao1 > populacao2) {
             printf("%s tem maior população que %s\n", Cidade1, Cidade2);
             } else {
                 printf("%s tem maior população que %s\n", Cidade2, Cidade1);
@@ -145,7 +187,7 @@ int main() {
             }
                 else 
                 printf ("Pois %s tem maior população, são %.2f habitantes.\n", Cidade2, populacao2);
-        
+*/
 
     return 0;
 }
